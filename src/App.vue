f65f5241b2044abdfea69ef7f76ea299a708b059
“石杰“ <shijie@cdvcloud.com>
2020-03-13 12:42:04 +0800

    合并one

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-<<<<<<< HEAD
-    <p>我是第二次</p>
-=======
     <p>我是第一次修改</p>
     <p>我是one新加的</p>
->>>>>>> one
   </div>
 </template>
 
